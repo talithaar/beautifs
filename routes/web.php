bcd6dbd0adb921feaf0f46504479eff5206cd93d
talithaar <talithaarr@gmail.com>
2023-05-18 13:04:59 +0700

    Mengunggah file proyek

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Route;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::controller(ProductController::class)->group(function(){
+    Route::get('/products', 'index');
+    Route::get('/products/create', 'create');
+});
